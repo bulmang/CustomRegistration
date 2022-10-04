af0c79da8fd4a1c6c8209eb66ac3ffd2ae6df97d
Bulmang <hmk113737@gmail.com>
2022-10-04 21:54:26 +0900

    Initial Commit

---
+++ CustomRegistration/CustomRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomRegistrationApp.swift
+//  CustomRegistration
+//
+//  Created by 하명관 on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct CustomRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
